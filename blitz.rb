24a0a040618e6f569f230038368b8657dd6f4f22
colinbankier <colinbankier@gmail.com>
2014-01-30 07:34:39 +1000

    Update supporter page and search api load tests.

--- blitz.rb
+++ blitz.rb
@@ -11,28 +11,34 @@ require 'blitz'
 # Or a Rush
 $default_timeout = 30000
 $default_headers = '--header "Accept-Encoding: gzip,deflate"'
-$pattern = '-p 1-50:120,50-50:60'
+$pattern = '-p 1-100:120,100-100:60'
 
 def charity_search_load_test
-  rush create_curl("-v:query a[1,8] -X GET https://edherow.com/api/v2/search/charities?q=\#{query}&country_code=us")
+  ["edherow.com", "edherox.com", "edheroy.com"].each do |host|
+    rush create_curl("-v:query a[1,8] https://#{host}/api/v2/search/charities?q=\#{query}&country_code=us")
+  end
 end
 
 def aggregate_search_load_test
-  rush create_curl("-v:query a[1,8] -X GET https://edherow.com/api/v2/search/aggregate?q=\#{query}&grouped=1&country_code=us")
+  ["edherow.com", "edherox.com", "edheroy.com"].each do |host|
+    rush create_curl("-v:query a[1,8] https://#{host}/api/v2/search/aggregate?q=\#{query}&group=1&country_code=us")
+  end
 end
 
 def view_supporter_page_load_test
   page_slugs = "[john-radcliffe,coralee-mary,grace-pritchard,michael-bohlscheid,david-gannon07-bigpond-com,kelly-haywood,playworksoz,walking4talking,claire-summerer,kiaana-brown]"
-  rush create_curl("-v:slug #{page_slugs} -X GET https://give.edherow.com/au/\#{slug}")
+  ["edherow.com", "edherox.com", "edheroy.com"].each do |host|
+    rush create_curl("-v:slug #{page_slugs} https://give.#{host}/au/\#{slug}")
+  end
 end
 
-def create_curl command, region = 'virginia'
-  "--timeout #{$default_timeout} #{$default_headers} --region #{region} --status 200 #{command}"
+def create_curl command
+  "--timeout #{$default_timeout} #{$default_headers} --status 200 #{command}"
 end
 
 def rush command
-  ['singapore', 'virginia'].each do |region|
-    rush = Blitz::Curl.parse "--region #{region} {$pattern} #{command}"
+  ['virginia'].each do |region|
+    rush = Blitz::Curl.parse "-r #{region} #{$pattern} #{command}"
     pp "Executing #{rush.inspect}"
     rush.execute do |partial|
       pp [ partial.region, partial.timeline.last.hits ]
@@ -48,5 +54,5 @@ def sprint command
 end
 
 charity_search_load_test
-# aggregate_search_load_test
-# view_supporter_page_load_test
+aggregate_search_load_test
+view_supporter_page_load_test
